932a8ce2b3dc8c51c85c495edf541467980d9466
Saban yilmaz <syilmaz002@ensc.fr>
2025-07-16 21:50:32 +0200

    Update LoginPage and SignUpPage components to replace logo with "My Dev Library" text for branding consistency; remove unused Image imports for cleaner code.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,21 +1,11 @@
 import { LoginForm } from "./login-form";
-import Image from "next/image";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-12 w-12 items-center justify-center rounded-md text-primary-foreground">
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={64}
-              height={64}
-              className=" text-white"
-            />
-          </div>
-          <span className="text-2xl font-bold"> Page Cadeau d&apos;Amour</span>
+          <span className="text-2xl font-bold">My Dev Library</span>
         </a>
         <LoginForm />
       </div>
